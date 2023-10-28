ea8a19f9f0afade8762e25cfd1ce8b20277f9460
NgTMDuc <ducntm23052002@gmail.com>
2023-10-29 00:00:32 +0700

    config

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ SYNTH_TRAIN_DIR2 = 'data/synthesis/'  # SynthText
 # PRIVATE_TEST_DIR = 'data/NAVER_OCR_private_test_update/'  # Private test
 # MODEL_PATH = 'saved_models'
 
-PRIVATE_TEST_DIR = '/kaggle/input/preprocess/'  # Private test
+PRIVATE_TEST_DIR = '/kaggle/input/preprocess/augmented/'  # Private test
 MODEL_PATH = '/kaggle/input/model-weights/saved_models/'
-
+# /kaggle/input/preprocess/augmented
 # For Kaggle
 # PUBLIC_TEST_DIR = '/kaggle/input/ocr-hackathon-v4/public_test_data/new_public_test'
 # TRAIN_DIR = '/kaggle/input/ocr-hackathon-v4/training_data/new_train'
